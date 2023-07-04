66ad7786664109ac2cb0d8a1349a3aa5939497a9
tony-brian <btoni3823@gmail.com>
2023-07-04 20:14:42 +0300

    commpleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution in this file!
+let customername = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+console.log(customerName); 
+
+let customerr
+Name = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+console.log(customerName); 
+
+
+let customerNamee = 'bob';
+function returnCustomerName() {
+  return customerName;
+}
+
+console.log(returnCustomerName()); 
+
+let customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+upperCaseCustomerName();
+console.log(customerName); 
+
+
+
